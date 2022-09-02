4e872b9fe6e7a45231052526c3f88ded37e04e53
Hrishabh963 <hrishabhkumar634@gmail.com>
2022-09-02 11:53:29 +0530

    Full Project Completed

---
+++ game.js
@@ -0,0 +1,92 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+var x = new Audio('sounds/background.mp3');
+x.play();
+$(document).keypress(function() {
+    if (!started) {
+        x.pause();
+        $("#level-title").text("Level " + level);
+        $("h1").removeClass("blink");
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function() {
+
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        $(".player-turn").addClass("hidden");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+
+    } else {
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h1").addClass("blink");
+        $("#level-title").text("Game Over, Press Any Key to Start");
+        startOver();
+    }
+
+}
+
+function nextSequence() {
+
+    setTimeout(function() {
+        $(".player-turn").removeClass("hidden");
+    }, 400)
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function startOver() {
+    setTimeout(function() {
+        x.currentTime = 0;
+        x.play();
+    }, 200)
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
